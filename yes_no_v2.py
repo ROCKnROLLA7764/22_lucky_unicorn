cf13f937770b04b9cf8c427b0f9f574fff92d012
ROCKnROLLA7764 <bb20084@melville-high.school.nz>
2022-08-18 14:20:34 +1200

    yes no working

---
+++ yes_no_v2.py
@@ -0,0 +1,15 @@
+show_instructions = ""
+while show_instructions.lower() != "xxx"
+    show_instructions = input("have you played this game before").lower().strip()
+
+
+    if show_instructions == "yes" or show_instructions == "Y":
+        print("program continues")
+    elif show_instructions == "y":
+        print("program continues")
+    elif show_instructions == "no":
+        print("display instructions")
+    elif show_instructions == "n":
+        print("display instructions")
+    elif:
+        print("please answer yes/no")
